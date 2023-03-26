1e27b23b28fa7ae4fcbf287e03c19bb3289828c6
Sujal Israni <128592707+TrickyToy13@users.noreply.github.com>
2023-03-26 16:05:37 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('form');
+form.addEventListener<'submit'>, (event) => {
+  event.preventDefault()
+  const name = document.querySelector('#name').value;
+  const email = document.querySelector('#email').value;
+  const team = document.querySelector('#team')
+}
+
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+rightnav=document.querySelector('.rightnav')
+navlist=document.querySelector('navlist')
+
+
+
+
+burger.addEventListener('.click',()>=r){
+rightnav.classlist.toggle('v-class')
+ navlist.classlist.toggle('v-class')
+ navbar.classlist.toggle('v-class')
+}
